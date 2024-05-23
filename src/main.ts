1423b59c65dace1ff345306491276b1f763d0b2f
Chris Kipp <ckipp@pm.me>
2024-05-23 15:53:06 +0200

    deps: bump default plugin version to 0.2.7

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {checkForValidMillVersion, getMillPath} from '../src/mill'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
-const defaultMillPluginVersion = '0.2.6'
+const defaultMillPluginVersion = '0.2.7'
 
 async function run(): Promise<void> {
   try {
